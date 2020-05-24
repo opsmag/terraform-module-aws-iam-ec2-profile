afa5907ba122f0cbfd2d09e2882837cd570e78df
Boris Karaoglanov <bkaraoglanov@astraflare.com>
2020-05-24 05:48:19 -0700

    Changed company_name to infrastructure

--- iam-role-policy.tf
+++ iam-role-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy" "this" {
-  name = "${var.project_name}-ec2-profile"
+  name = "${var.infrastructure}-ec2-profile"
   role = aws_iam_role.this.*.id[0]
 
   policy = <<EOF
